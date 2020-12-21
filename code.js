14665be9121c8e2267cb6d59ddfcd7cd289987a9
jonarivado <jonathan.dotzler@gmail.com>
2020-12-21 18:50:03 +0100

    added resume & fixed nav color

--- code.js
+++ code.js
@@ -8,7 +8,35 @@ function closeMobileNav() {
   }
 
   //Listen to scroll
-  window.addEventListener('scroll', () => {
+
+  window.addEventListener('scroll', () => { 
     document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  
+   }, false);
+
+  window.addEventListener('scroll', () => {
+    var someDiv = document.getElementById('resume');
+   var distanceToTop = someDiv.getBoundingClientRect().top;
+   if(distanceToTop <= 0 && distanceToTop > -someDiv.clientHeight) {
+     document.getElementById('mynav').style.color = 'white';
+   }
+   else {
+    document.getElementById('mynav').style.color = 'black';
+   }
+
+   if(isElementInViewport(document.getElementById('edu'))) {
+    document.getElementById('edu').classList.add('animate__animated', 'animate__slideInUp');
+
+   }
+
   }, false);
 
+  function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+     rect.top >= 0 &&
+     rect.left >= 0 &&
+     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+   }
