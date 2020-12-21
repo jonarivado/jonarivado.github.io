69ffa8a78c059875607fe6d8111bb75dea035b76
jonarivado <jonathan.dotzler@gmail.com>
2020-12-21 13:04:23 +0100

    added parts of resume

--- code.js
+++ code.js
@@ -9,12 +9,12 @@ function closeMobileNav() {
 
   //Listen to scroll
   window.addEventListener('scroll', () => {
+    if(window.scrolltop == 210) {
+      document.getElementById('mynav').style.background = "#ccc";
+    }
+    else {
+      document.getElementById('mynav').style.background = "transparent";
+    }
     document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
 
-  window.addEventListener('scroll', () => {
-    if(scroll >= document.body.offsetHeight) {
-      document.getElementById('mynav').style.background='#000000';
-    }
-  })
-  
