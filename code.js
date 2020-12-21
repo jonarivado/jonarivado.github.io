1a31099fbb2ee9713e7c7976296c21cb5ce2a20c
jonarivado <jonathan.dotzler@gmail.com>
2020-12-21 13:18:23 +0100

    temp fix for nav

--- code.js
+++ code.js
@@ -9,12 +9,6 @@ function closeMobileNav() {
 
   //Listen to scroll
   window.addEventListener('scroll', () => {
-    if(window.scrolltop == 210) {
-      document.getElementById('mynav').style.background = "#ccc";
-    }
-    else {
-      document.getElementById('mynav').style.background = "transparent";
-    }
     document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
 
